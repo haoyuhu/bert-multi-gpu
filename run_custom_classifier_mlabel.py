8bdbc3c4619ed8ece1f465ab0cc5aa1a948150b3
hahahu <im@huhaoyu.com>
2020-03-09 23:11:06 +0800

    fixbug: key error, 'batch_size' not in params

--- run_custom_classifier_mlabel.py
+++ run_custom_classifier_mlabel.py
@@ -436,7 +436,7 @@ def file_based_input_fn_builder(input_file, seq_length, is_training,
 
     def input_fn(params):
         """The actual input function."""
-        batch_size = params["batch_size"]
+        # batch_size = params["batch_size"]
 
         # For training, we want a lot of parallel reading and shuffling.
         # For eval, we want no shuffling and parallel reading doesn't matter.
